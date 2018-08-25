d7ae0e7d438c119f10814166fad37362d2cf8d93
Albert Chun <albert.s.chun@gmail.com>
2018-08-25 22:44:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 function dwarfRollCall(dwarves) {
+  var nameIndexString = "";
+  dwarves.forEach(function(name, index) {
+  	nameIndexString += [index+1] + ". " + name + " ";
+  });
+  return nameIndexString;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  //using arrow function syntax 
+  let newPlanet = planeteerCalls.map(planet => planet.toUpperCase() + '!');
+  return newPlanet;
 }
 
 function longPlaneteerCalls(words) {
+  for (let wordsIndex = 0; wordsIndex < words.length; wordsIndex++) {
+    if (words[wordsIndex].length > 4) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 function findTheCheese (foods) {
+  
 }
